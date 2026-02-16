25782f3990d9188105b024bca10d270cc92ce368
jesse ezell <jezell@gmail.com>
2026-02-16 12:28:22 -0800

    improve responder

--- lib/meshagent/thread_view.dart
+++ lib/meshagent/thread_view.dart
@@ -56,7 +56,7 @@ class MeshagentRoomChatThreadController extends ChatThreadController {
 
       bool added = false;
       void waitForAgent() async {
-        if (agentName != null && room.messaging.remoteParticipants.where((x) => x.getAttribute("namme") == agentName).isEmpty) {
+        if (agentName != null && room.messaging.remoteParticipants.where((x) => x.getAttribute("name") == agentName).isEmpty) {
           if (!added) {
             room.messaging.addListener(waitForAgent);
             added = true;
