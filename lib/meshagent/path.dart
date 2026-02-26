3d040d494547f58b4ae0e3ab64ba0513ebb5404b
Owen Garmire <owen.garmire@timu.com>
2026-02-25 20:59:24 -0800

    fix file table. add file table multi select/delete.

--- lib/meshagent/path.dart
+++ lib/meshagent/path.dart
@@ -1,3 +1,9 @@
 String joinPaths(String s1, String s2) {
   return [...s1.split("/"), ...s2.split("/")].where((x) => x.isNotEmpty).join("/");
 }
+
+String parentPath(String s) {
+  final normalized = s.endsWith('/') ? s.substring(0, s.length - 1) : s;
+  final index = normalized.lastIndexOf('/');
+  return index <= 0 ? '' : normalized.substring(0, index);
+}
